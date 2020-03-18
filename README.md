7acadf0d2404bade4f1ef871ae7cd39970bf2520
Jan Keromnes <janx@linux.com>
2020-03-18 15:30:28 +0000

    Add helper script for adding extensions, use it to add a bunch of popular extensions

--- README.md
+++ README.md
@@ -3,3 +3,30 @@
 [![Gitpod Ready-to-Code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/open-vsx/publish-extensions)
 
 CI for publishing open-source VS Code extensions to https://open-vsx.org
+
+## Adding an extension
+
+To automatically publish an extension to Open VSX, simply add it to [`extensions.json`](./extensions.json).
+
+You can also run `node add-extension [REPOSITORY]` to add it automatically.
+
+Here is the expected format of an `extensions.json` entry:
+
+```js
+    {
+      "id": "redhat.vscode-yaml", // Unique Open VSX extension ID in the form "<namespace>.<name>"
+      "repository": "https://github.com/redhat-developer/vscode-yaml" // Repository URL to clone and publish from
+    },
+```
+
+Here are all the supported values, including optional ones:
+
+```js
+    {
+      "id": "ms-vscode.cpptools", // Unique Open VSX extension ID in the form "<namespace>.<name>"
+      "version": "0.26.3", // [OPTIONAL] The version to publish to Open VSX (defaults to the "version" in your package.json)
+      "repository": "https://github.com/microsoft/vscode-cpptools", // Repository URL to clone and publish from
+      "checkout": "0.26.3", // [OPTIONAL] The Git branch, tag, or commit to check out before publishing (defaults to the default Git branch in the repository)
+      "location": "Extension" // [OPTIONAL] Location of your extension's package.json in the repository (defaults to ".")
+    },
+```
---
+++ add-extension.js
@@ -0,0 +1,82 @@
+/********************************************************************************
+ * Copyright (c) 2020 TypeFox and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
+
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const exec = require('./lib/exec');
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+(async () => {
+  const argv = process.argv.slice(2);
+  const { extensions } = JSON.parse(await readFile('./extensions.json', 'utf-8'));
+
+  if (argv.length !== 1) {
+    console.log('Usage: node add-extension [REPOSITORY]');
+    process.exit();
+  }
+
+  const repository = argv[0];
+  const existing = extensions.find(extension => extension.repository.toLowerCase() === repository.toLowerCase());
+  if (existing) {
+    console.log(`[SKIPPED] Repository already in extensions.json: ${JSON.stringify(existing, null, 2)}`);
+    return;
+  }
+
+  try {
+    if (!new URL(repository)) {
+      throw new Error(`Invalid repository URL: ${repository}`);
+    }
+
+    // Clone the repository to determine the extension's latest version.
+    await exec(`git clone ${repository} /tmp/repository`);
+
+    // Locate and parse package.json.
+    const { stdout: files } = await exec('ls package.json 2>/dev/null || git ls-files | grep package\\.json', { cwd: '/tmp/repository' });
+    if (!files.trim()) {
+      throw new Error(`No package.json found in repository!`);
+    }
+    const locations = files.trim().split('\n');
+    if (locations.length > 1) {
+      console.warn(`[WARNING] Multiple package.json found in repository, arbitrarily using the first one:\n> ${locations[0]}\n${locations.slice(1).map(l => '  ' + l).join('\n')}`);
+    }
+    const package = JSON.parse(await readFile(path.join('/tmp/repository', locations[0]), 'utf-8'));
+    ['publisher', 'name', 'version'].forEach(key => {
+      if (!(key in package)) {
+        throw new Error(`Expected "${key}" in ${locations[0]}: ${JSON.stringify(package, null, 2)}`);
+      }
+    });
+
+    // Add extension to the list.
+    const extension = { id: `${package.publisher}.${package.name}`, version: package.version, repository };
+    const location = path.dirname(locations[0]);
+    if (location !== '.') {
+      extension.location = location;
+    }
+    extensions.push(extension);
+
+    // Sort extensions alphabetically by ID.
+    extensions.sort((a, b) => {
+      if (b.id > a.id) return -1;
+      if (b.id < a.id) return 1;
+      return 0;
+    });
+
+    // Save new extensions list.
+    await writeFile('./extensions.json', JSON.stringify({ extensions }, null, 2), 'utf-8');
+    console.log(`[OK] Succesfully added new extension: ${JSON.stringify(extension, null, 2)}`);
+  } catch (error) {
+    console.error(`[FAIL] Could not add ${repository}!`);
+    console.error(error);
+  } finally {
+    await exec('rm -rf /tmp/repository');
+  }
+})();
