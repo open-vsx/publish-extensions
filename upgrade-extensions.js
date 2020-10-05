f732b7eccaa19cbb65b0ffbcfae69f7fc59dcf24
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2020-10-04 23:21:33 -0500

    Ignore releases which have `-insider` in their version string.

--- upgrade-extensions.js
+++ upgrade-extensions.js
@@ -65,7 +65,7 @@ const dontUpgrade = [
         const releasesUrl = extension.download.replace(/\/releases\/download\/.*$/, '/releases');
         console.log(`Scraping ${releasesUrl} to check for updates...`);
         const releases = await get(releasesUrl);
-        const latest = releases.match(/\/releases\/download\/[-._a-zA-Z0-9\/%]*\.vsix/g).filter(release => !/(nightly|-rc|-alpha|-beta)/.test(release)).shift();
+        const latest = releases.match(/\/releases\/download\/[-._a-zA-Z0-9\/%]*\.vsix/g).filter(release => !/(nightly|-rc|-alpha|-insider|-beta)/.test(release)).shift();
         await exec('node add-extension --download=' + (latest ? extension.download.replace(/\/releases\/download\/.*$/, latest) : extension.download));
     }
 
