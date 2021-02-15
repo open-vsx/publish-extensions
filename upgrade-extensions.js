5467c807519609ffbf56f6483d1bdf2232beb254
Jan Keromnes <jan.keromnes@typefox.io>
2021-02-15 11:00:14 +0100

    Don't auto-upgrade ms-vscode.hexeditor

--- upgrade-extensions.js
+++ upgrade-extensions.js
@@ -31,6 +31,7 @@ const dontUpgrade = [
   'ms-vscode.node-debug', // Recent releases are not being tagged in https://github.com/microsoft/vscode-node-debug
   'wmaurer.change-case', // https://github.com/wmaurer/vscode-change-case/releases didn't have a release in 6 years
   'jebbs.plantuml', // https://github.com/open-vsx/publish-extensions/issues/272 and https://github.com/open-vsx/publish-extensions/pull/290/files#r576063404
+  'ms-vscode.hexeditor', // https://github.com/open-vsx/publish-extensions/pull/290/files#r576063874
 ];
 
 (async () => {
