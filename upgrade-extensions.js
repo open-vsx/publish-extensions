e159c75a6636af164bf6c4d795c0a22684b1d10f
Jan Keromnes <jan.keromnes@typefox.io>
2021-02-15 10:58:41 +0100

    upgrade-extensions: Don't auto-upgrade jebbs.plantuml

--- upgrade-extensions.js
+++ upgrade-extensions.js
@@ -30,6 +30,7 @@ const dontUpgrade = [
   'ms-vscode.atom-keybindings', // https://github.com/microsoft/vscode-atom-keybindings/releases didn't have a release in 2 years
   'ms-vscode.node-debug', // Recent releases are not being tagged in https://github.com/microsoft/vscode-node-debug
   'wmaurer.change-case', // https://github.com/wmaurer/vscode-change-case/releases didn't have a release in 6 years
+  'jebbs.plantuml', // https://github.com/open-vsx/publish-extensions/issues/272 and https://github.com/open-vsx/publish-extensions/pull/290/files#r576063404
 ];
 
 (async () => {
