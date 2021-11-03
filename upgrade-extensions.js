fc023d89eb87b22e7e382ca88361ff60ef0276e6
Filip Troníček <filip@gitpod.io>
2021-11-03 19:54:15 +0000

    Check if version is not lower than the already specified one

--- upgrade-extensions.js
+++ upgrade-extensions.js
@@ -87,7 +87,7 @@ const dontUpgrade = [
       try {
         // Scrape the latest GitHub releases to check for updates.
         const repository = extension.download.replace(/\/releases\/download\/.*$/, '');
-        const latest = await gitHubScraper.findLatestVSIXRelease(repository);
+        const latest = await gitHubScraper.findLatestVSIXRelease(repository, extension.version);
         await exec('node add-extension --download=' + (latest || extension.download));
       } catch (e) {
         console.error(`${extension.id}: failed to upgrade downloads:`, e);
